23d1949844d097b9087c1f706e311695239a424c
Bartho <23091029@student.hhs.nl>
2024-04-12 09:10:20 +0200

    new game object

--- Main.java
+++ Main.java
@@ -3,4 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Game cod = new Game("modern warfare",69.99);
     }
+    public void getReviewGame(Game game) {
+
+    }
+    public void getGameRanglijst() {
+
+    }
+    public void getKortingGames() {
+
+    }
+    public void login(String username) {
+
+    }
+
 }
