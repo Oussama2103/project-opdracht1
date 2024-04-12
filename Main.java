b20065fb5ae6746a70df99600c4422497ce55866
Bartho <23091029@student.hhs.nl>
2024-04-12 11:13:16 +0200

    new game objects

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@
 public class Main {
     public static void main(String[] args) {
         Game cod = new Game("modern warfare",69.99);
+        Game bf3 = new Game("Battlefield 3", 34.00);
+        Game celeste = new Game("Celeste",19.50);
+        Game hk = new Game("Hollow Knight",14.79);
+        Game aL = new Game("Apex Legends", 0.00);
+        Game spdrmn = new Game("Spider Man", 59.99);
+        Game lc = new Game ("Lethal Company", 9.75);
+        Game eldnrng = new Game("Elden Ring",59.99);
+        Game mrkrt = new Game("Mario Kart 8 Deluxe", 50.00);
+        Game bdborne = new Game("Bloodborne", 25.00);
+        Game jstdnce = new Game("Just Dance", 30.00);
     }
     public void getReviewGame(Game game) {
 
