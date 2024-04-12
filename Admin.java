c9a90ee7e28c1ffb0e874dd39f1db2253433d074
hwahi <22033491@student.hhs.nl>
2024-04-12 10:49:54 +0200

    p

--- Admin.java
+++ Admin.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Admin {
     private Gebruiker gebruiker;
 
     public Admin (Gebruiker gebruiker) {
         this.gebruiker = gebruiker;
+    }
+    private ArrayList<Review> reviewLijst;
+
+    public Admin() {
+        reviewLijst = new ArrayList<>();
+    }
+
+    public void voegReviewToe(Review review) {
+        reviewLijst.add(review);
+    }
+
+    public List<Review> bekijkReviews() {
+        return new ArrayList<>(reviewLijst);
+    }
+
+    public void geefReview(int graphicScore, int gameplayScore, int storylineScore, String feedback, Vragenlijst vragenlijst) {
+        Review review = new Review(graphicScore, gameplayScore, storylineScore, feedback, vragenlijst);
+        voegReviewToe(review);
     }
 }
--- Gebruiker.java
+++ Gebruiker.java
@@ -4,21 +4,21 @@ public class Gebruiker {
     private String username;
     private Review review;
 
-    public Gebruiker (String username, Review review) {
+    public Gebruiker(String username, Review review) {
         this.username = username;
         this.review = review;
     }
 
-    public String getUsername () {
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername (String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public void bekijkReviews(Eigenaar eigenaar) {
-        List<Review> reviews = eigenaar.bekijkReviews();
+    public void bekijkReviews(Admin admin) {
+        List<Review> reviews = admin.bekijkReviews();
         for (Review review : reviews) {
             System.out.println("graphicScore: " + review.getGraphicsScore());
             System.out.println("gameplayScore: " + review.getGameplayScore());
